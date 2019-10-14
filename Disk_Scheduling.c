34009b23aed88a92f670ba2a9c00d13b1a0cec0a
Augustine S Aykara <augustineaykara@gmail.com>
2019-10-14 23:46:53 +0530

    FCFS

--- Disk_Scheduling.c
+++ Disk_Scheduling.c
@@ -3,10 +3,27 @@
 
 #include<stdio.h>
 #include <stdlib.h>
+int seektime = 0;
+
+void fcfs(int sequence[], int pos, int n)
+{				
+	int temp = pos;
+	printf("\n The Disk sequence is : \n");
+	for(int i=0; i<n; i++)
+	{
+		printf(" > %d", sequence[i]);
+		seektime += abs(sequence[i]-temp);
+		temp = sequence[i];
+	}
+
+	printf("\n\n Seek Time = %d", seektime);
+	printf("\n\n");
+}
+
 
 void main()
 {
-	int n, t, i, pos, temp, seektime = 0;
+	int n, t, i, pos, temp, choice;
 	printf("\n Enter number of disk request in queue : ");
 	scanf("%d", &n);
 	printf(" Enter total number of tracks : ");
@@ -21,15 +38,14 @@ void main()
 	printf(" Enter the initial position of the R/W head : ");
 	scanf("%d", &pos);
 
-	temp = pos;
-	printf("\n The Disk sequence is : \n");
-	for(i=0; i<n; i++)
-	{
-		printf(" > %d", sequence[i]);
-		seektime += abs(sequence[i]-temp);
-		temp = sequence[i];
-	}
+	printf("\n 1. FCFS \n 2. SSTF \n 3. SCAN \n 4.PRINT ALL");
+	printf(" Enter your choice : ");
+	scanf("%d", &choice);
 
-	printf("\n\n Seek Time = %d", seektime);
-	printf("\n\n");
+	switch(choice)
+	{
+		case 1: fcfs(sequence, pos, n);
+				break;
+				
+	}
 }
