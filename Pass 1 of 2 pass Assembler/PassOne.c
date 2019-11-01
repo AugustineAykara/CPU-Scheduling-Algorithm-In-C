c4b285adef5ec881312b06dd54a37f8c6d8e2b07
Augustine S Aykara <augustineaykara@gmail.com>
2019-11-02 00:25:16 +0530

    Update PassOne.c

--- Pass 1 of 2 pass Assembler/PassOne.c
+++ Pass 1 of 2 pass Assembler/PassOne.c
@@ -7,21 +7,19 @@
 
 void main()
 {
-	char opcode[20], operand[20], label[20], code[20], menumonic[20];
-	int locctr, start, lenght;
-
+	char label[20], opcode[20], operand[20], code[20], length[20];
+	int locctr;
 	FILE *f1, *f2, *f3, *f4;
 
-	f1 = fopen("Input.txt", "r");
-	f2 = fopen("Optab.txt", "r");
-	f3 = fopen("Output.txt", "w");
-	f4 = fopen("Symtab.txt", "w");
+	f1 = fopen("Input.dat", "r");
+	f2 = fopen("Optab.dat", "r");
+	f3 = fopen("Output.dat", "w");
+	f4 = fopen("Symtab.dat", "w");
 
 	fscanf(f1, "%s\t%s\t%s", label, opcode, operand);
 	if(strcmp(opcode, "START") == 0)
 	{
-		start = atoi(operand);
-		locctr = start;
+		locctr = atoi(operand);
 		fprintf(f3, "\t\t%s\t%s\t%s\n", label, opcode, operand);
 		fscanf(f1, "%s\t%s\t%s", label, opcode, operand);
 	}
@@ -36,18 +34,19 @@ void main()
 
 		if(strcmp(label, "*****") != 0)
 		{
-			fprintf(f4, "%s\t%d\n", label, locctr);
+			fprintf(f4, "%d\t%s\n", locctr, label);
 		}
-		fscanf(f2, "%s\t%s", code, menumonic);
+		fscanf(f2, "%s\t%s", code, length);
 	
 		while(strcmp(code, "END") != 0)
 		{
 				if(strcmp(opcode, code) == 0)
 				{
 					locctr += 3;
+					rewind(f2);
 					break;
 				}
-				fscanf(f2, "%s\t%s", code, menumonic);
+				fscanf(f2, "%s\t%s", code, length);
 		}
 
 		if(strcmp(opcode, "RESW") == 0)
@@ -70,8 +69,6 @@ void main()
 		fscanf(f1, "%s\t%s\t%s", label, opcode, operand);
 	}
 	fprintf(f3, "%d\t%s\t%s\t%s\n", locctr, label, opcode, operand);
-	lenght = locctr - start;
-	printf("\n Length = %d \n", lenght);
 
 	fclose(f1);
 	fclose(f2);
