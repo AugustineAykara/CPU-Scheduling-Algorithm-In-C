79185d641a8c9aac126f93bc0db368c21c5d752c
Augustine S Aykara <augustineaykara@gmail.com>
2019-11-12 15:14:53 +0530

    Update AbsoluteLoader.c

--- Absolute Loader/AbsoluteLoader.c
+++ Absolute Loader/AbsoluteLoader.c
@@ -1,1 +1,40 @@
-
+# include <stdio.h>
+# include <string.h>
+void main()
+{
+	char input[10];
+	int start, length, address;
+	FILE *fp1, *fp2;
+	
+	fp1=fopen("Input.dat","r");
+	fp2=fopen("Output.dat","w");
+	
+	fscanf(fp1,"%s",input);
+	while(strcmp(input,"E")!=0)
+	{
+		if(strcmp(input,"H") == 0)
+		{
+			fscanf(fp1,"%d",&start);
+			fscanf(fp1,"%d",&length);
+		}
+		else if(strcmp(input,"T") == 0)
+		{
+			fscanf(fp1,"%d",&address);
+			fscanf(fp1,"%s",input);
+			fprintf(fp2,"%d\t%c%c\n",address,input[0],input[1]);
+			fprintf(fp2,"%d\t%c%c\n",(address+1),input[2],input[3]);
+			fprintf(fp2,"%d\t%c%c\n",(address+2),input[4],input[5]);
+			address+=3;
+		}
+		else
+		{
+			fprintf(fp2,"%d\t%c%c\n",address,input[0],input[1]);
+			fprintf(fp2,"%d\t%c%c\n",(address+1),input[2],input[3]);
+			fprintf(fp2,"%d\t%c%c\n",(address+2),input[4],input[5]);
+			address+=3;
+		}
+		fscanf(fp1,"%s",input);
+	}
+	fclose(fp1);
+	fclose(fp2);
+}
